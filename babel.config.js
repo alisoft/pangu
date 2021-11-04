8236a4f8fa2ec58efc40d57f4485322da9a01bfe
realkimmy <upcwangying@126.com>
2021-11-04 22:14:23 +0800

    fix: `test:unit`.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
   plugins: [
     [
       "import",
