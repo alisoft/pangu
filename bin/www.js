4505b2d2f1852e00bac1c1aaa0ef34074cf75712
realkimmy <upcwangying@126.com>
2021-11-26 19:42:46 +0800

    Add `www.js`.

---
+++ bin/www.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const app = require("../src/node/app");
+const config = require("../src/node/config/config");
+const logger = require("../src/node/config/logger");
+const http = require("http");
+
+app.set("port", config.port);
+const server = http.createServer(app);
+
+server.listen(config.port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind =
+    typeof config.port === "string"
+      ? "Pipe " + config.port
+      : "Port " + config.port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      logger.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      logger.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+    logger.info("Connected to MongoDB");
+  });
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  logger.info("Listening on " + bind);
+}
