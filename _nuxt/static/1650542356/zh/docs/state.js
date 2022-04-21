d88f9c2a3b495e06f08d9f2e692afe5a0b47fbb2
see <upcwangying@126.com>
2022-04-21 12:00:58 +0000

    deploy: afefa017a6a183737a948c46c00e3fdf01497f71

---
+++ _nuxt/static/1650542356/zh/docs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650542356",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fzh\u002Fdocs",config:{apiURL:b,plausible:{domain:b},recaptcha:{version:3,siteKey:b},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},docusDbHash:"dca5629f"},globalRefs:{}}}(null,void 0));
