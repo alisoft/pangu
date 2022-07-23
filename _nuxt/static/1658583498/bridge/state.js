f63e98570d9ea53b5b6bec6e02ad1b3b2f78d0a0
see <upcwangying@126.com>
2022-07-23 13:39:35 +0000

    deploy: ee81e3f6686ae3a5e5a69d878e267cf9f1bd1062

---
+++ _nuxt/static/1658583498/bridge/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658583498",layout:"default",error:a,serverRendered:false,routePath:"\u002Fbridge",config:{plausible:{domain:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},docusDbHash:"e7f8fdfc"},globalRefs:{}}}(null));
